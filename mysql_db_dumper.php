699d7a0412ffcf0ba4784f33c51591fe582cacf1
Svetoslav Marinov (Slavi) <slavi@orbisius.com>
2016-10-06 18:07:24 +0300

    * The output dir can contain placeholders e.g. YYYY-MM-DD or DAY_OF_WEEK which are dynamically replaced which are nice for weekly backups.

--- mysql_db_dumper.php
+++ mysql_db_dumper.php
@@ -5,14 +5,35 @@
  * php mysql_db_dumper.php db_root_user db_root_pass output_dir
  * 
  * php mysql_db_dumper.php "" "" C:\Copy\Dropbox\cloud\db_export
+ *
+ * The output dir can contain placeholders e.g. YYYY-MM-DD or DAY_OF_WEEK which are dynamically replaced which are nice for weekly backups.
+ * php "C:\Copy\Dropbox\cloud\projects\default\htdocs\github\orbisius-github-projects\server-tools\mysql_db_dumper.php" "root" "root_db_pass" C:\Copy\Dropbox\cloud\db_export\YYYY-MM\DAY_OF_WEEK\
+ *
  * @author Svetoslav Marinov (Slavi) | orbisius.com
  * @copyright 2016
  */
 $user = empty( $argv[1] ) ? 'root' : $argv[1];
 $pass = empty( $argv[2] ) ? '' : $argv[2];
-$out_dir = empty( $argv[3] ) ? dirname( __FILE__ ) . '/db_export/' . date( 'Y-m-d/' ) : $argv[3];
+$out_dir = empty( $argv[3] ) ? dirname( __FILE__ ) . '/db_export/YYYY-MM-DD/' : $argv[3];
 $out_dir = rtrim( $out_dir, '/\\');
 $out_dir .= "/";
+
+// If the path or export dir contains something that looks like YYYY-MM-DD it will be automatically
+// replaced with the current date.
+if ( preg_match( '#YYYY\-MM\-DD#si', $out_dir ) ) {
+    // The order is super important!
+	$out_dir = preg_replace( '#\bYYYY\-MM\-DD\b#si', date( 'Y-m-d' ), $out_dir );
+	$out_dir = preg_replace( '#\bYYYY\-MM\b#si', date( 'Y-m' ), $out_dir );
+	$out_dir = preg_replace( '#\bYYYY\b#si', date( 'Y' ), $out_dir );
+}
+
+// This is good if you want to have a weekly backup
+// $0 root pass c:\backups\db_export\YYYY-MM\DAY_OF_WEEK\ ->
+$day_of_week_regex = '#WEEK[_\-]DAY|DAY[_\-]OF[_\-](?:the[_\-]?)?WEEK#si';
+
+if ( preg_match( $day_of_week_regex, $out_dir ) ) {
+    $out_dir = preg_replace( $day_of_week_regex, strtolower( date( 'l' ) ), $out_dir );
+}
 
 if ( ! is_dir( $out_dir ) ) {
     if ( ! mkdir( $out_dir, 0755, 1 ) ) {
@@ -28,6 +49,7 @@ $db_user_esc = escapeshellarg( $user );
 $db_pass_esc = escapeshellarg( $pass );
 
 echo "Starting at: " . date('r') . "\n";
+echo "Export dir: [$out_dir]\n";
 
 $db_list = `$mysql --user=$db_user_esc --password=$db_pass_esc -Ns -e "SHOW DATABASES" 2>&1`;
 
