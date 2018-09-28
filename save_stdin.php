6648884e47907eb85f88432155580d9dd441c739
Slavi <slavi@orbisius.com>
2018-09-29 02:27:26 +0300

    using dirname and specifying which file can't be written to

--- save_stdin.php
+++ save_stdin.php
@@ -9,47 +9,47 @@
  */
 
 try {
-    $exit_code = 0; // success by default
-    $bytes_read = 0;
+	$exit_code = 0; // success by default
+	$bytes_read = 0;
 
-    if (php_sapi_name() == 'cli') {
-        $in = fopen('php://stdin', 'rb');
-    } else {
-        $in = fopen('php://input', 'rb');
-    }
+	if (php_sapi_name() == 'cli') {
+		$in = fopen('php://stdin', 'rb');
+	} else {
+		$in = fopen('php://input', 'rb');
+	}
 
-    if (empty($in)) {
-        throw new Exception("Cannot open STDIN for reading.");
-    }
+	if (empty($in)) {
+		throw new Exception("Cannot open STDIN for reading.");
+	}
 
-    $output_file = empty($argv[1]) ? __DIR__ . '/aaa_stdin_data_capture_' . microtime(true) . '.' . mt_rand(9999, 99999) . '.log' : $argv[1];
+	$output_file = empty($argv[1]) ? dirname(__FILE__) . '/aaa_stdin_data_capture_' . microtime(true) . '.' . mt_rand(9999, 99999) . '.log' : $argv[1];
 
-    $out = fopen($output_file, 'wb');
+	$out = fopen($output_file, 'wb');
 
-    if (empty($out)) {
-        throw new Exception("Cannot open output file for writing.");
-    }
+	if (empty($out)) {
+		throw new Exception("Cannot open output file [$output_file] for writing.");
+	}
 
-    $lock_status = flock($out, LOCK_EX); // blocking?
+	$lock_status = flock($out, LOCK_EX); // blocking?
 
-    if (empty($lock_status)) {
-        throw new Exception("Cannot lock output file for writing.");
-    }
+	if (empty($lock_status)) {
+		throw new Exception("Cannot lock output file for writing.");
+	}
 
-    while (!feof($in)) {
-        $buff = fread($in, 8192);
-        fwrite($out, $buff);
-        $bytes_read += strlen($buff);
-    }
+	while (!feof($in)) {
+		$buff = fread($in, 8192);
+		fwrite($out, $buff);
+		$bytes_read += strlen($buff);
+	}
 
-    flock($out, LOCK_UN);
-    fclose($in);
-    fclose($out);
-    fwrite(STDERR, "STDERR: Output file: [$output_file]\n");
-    fwrite(STDERR, "STDERR: Bytes read: [$bytes_read]\n");
+	flock($out, LOCK_UN);
+	fclose($in);
+	fclose($out);
+	fwrite(STDERR, "STDERR: Output file: [$output_file]\n");
+	fwrite(STDERR, "STDERR: Bytes read: [$bytes_read]\n");
 } catch (Exception $e) {
-    fwrite(STDERR, $e->getMessage() . "\n");
-    $exit_code = 255;
+	fwrite(STDERR, $e->getMessage() . "\n");
+	$exit_code = 255;
 }
 
 exit($exit_code);
